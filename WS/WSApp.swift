8bbafd95b3c620eea8116e53f5d1dc311132de4e
Anton Serdyuchenko <anton415@gmail.com>
2024-12-21 14:18:02 +0300

    Initial Commit

---
+++ WS/WSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WSApp.swift
+//  WS
+//
+//  Created by Anton Serdyuchenko on 21.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct WSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
